cfa44ccae8db279bd659cd473af437b8c22a47a6
krward2 <krward2@miners.utep.edu>
2020-09-02 20:11:45 -0600

    functional

---
+++ wordCount.py
@@ -0,0 +1,12 @@
+import sys        # command line arguments
+import re         # regular expression tools
+import os         # checking if file exists
+import subprocess # executing program
+
+def validNumberofArgs():
+    if len(sys.argv) =! 3:
+        
+
+def main():
+
+main()
