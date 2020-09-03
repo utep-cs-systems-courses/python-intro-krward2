41c036b373ad867a1ace1f849eeabff26c57afc1
krward2 <krward2@miners.utep.edu>
2020-09-02 20:54:24 -0600

    Filters punctuation now

--- wordCount.py
+++ wordCount.py
@@ -1,12 +1,32 @@
 import sys        # command line arguments
-import re         # regular expression tools
+import string     # string manipulation and formatting
 import os         # checking if file exists
-import subprocess # executing program
 
-def validNumberofArgs():
-    if len(sys.argv) =! 3:
-        
+if len(sys.argv) != 3:
+    print("Correct usage is: python wordCount.py input.txt output.txt")
+    exit()
 
-def main():
+input = sys.argv[1]
+output = sys.argv[2]
 
-main()
+if not os.path.exists(input):
+    print("Input text file path is not valid.")
+    exit()
+
+wordCounter = dict()
+
+with open(input,"r") as inputFile:
+    for line in inputFile:
+        line = line.lower()
+        line = line.translate(str.maketrans('', '', string.punctuation))
+        for word in line.split():
+            if word in wordCounter:
+                wordCounter[word] += 1
+            else:
+                wordCounter[word] = 1
+
+sortedDict = sorted(wordCounter.items(), key = lambda x: x[1], reverse = True)
+
+with open(output, "w") as outputFile:
+    for i in sortedDict:
+        outputFile.write(i[0] + " " + str(i[1]) + "\n")
